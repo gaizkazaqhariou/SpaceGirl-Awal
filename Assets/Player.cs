b23305d92e7c79487e791c6e26d3e831655b5b27
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-02-25 07:42:29 +0700

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	public Text starText;
+	public Image starImage;
+	public Sprite iconStar;
+	public Sprite iconNoStar;
+	private bool carryingStar = false;
+	// Use this for initialization
+	void Start () {
+		UpdateStarText ();
+	}
+	
+	void OnTriggerEnter2D(Collider2D hit){
+		if(hit.CompareTag("star")){
+			carryingStar = true;
+			UpdateStarText();
+			UpdateStarImage();
+			Destroy(hit.gameObject);
+		}
+	}
+	private void UpdateStarText(){
+		string starMessage = "Tidak bawa bintang";
+		if(carryingStar) starMessage = "Bawa bintang";
+		starText.text = starMessage;
+	}
+	private void UpdateStarImage(){
+		if (carryingStar) {
+			starImage.sprite = iconStar;
+		} else {
+			starImage.sprite = iconNoStar;
+		}
+	}
+}
